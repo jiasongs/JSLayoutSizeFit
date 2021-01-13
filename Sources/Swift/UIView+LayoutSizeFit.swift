a86ed09154ba883652ec1f4eff16a823f6ff6be7
jiasong <593908937@qq.com>
2021-01-14 00:00:25 +0800

    F

--- Sources/Swift/UIView+LayoutSizeFit.swift
+++ Sources/Swift/UIView+LayoutSizeFit.swift
@@ -13,7 +13,7 @@ public extension LayoutSizeFitWrapper where Base: UIView {
         return self.base.js_isUseFrameLayout
     }
     
-    mutating func useFrameLayout(_ isUse: Bool) -> Void {
+    func useFrameLayout(_ isUse: Bool) -> Void {
         self.base.js_isUseFrameLayout = isUse
     }
     
