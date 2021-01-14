6df8ea62a5599511780a5cc633cb72d1d41c4f70
jiasong <593908937@qq.com>
2021-01-14 08:29:15 +0800

    FIX

---
+++ Example/JSLayoutSizeFitExample/SwiftViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftViewController.swift
+//  JSLayoutSizeFitExample
+//
+//  Created by jiasong on 2021/1/14.
+//  Copyright © 2021 jiasong. All rights reserved.
+//
+
+import UIKit
+import JSLayoutSizeFit
+
+class SwiftViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tableView = UITableView()
+        tableView.lsf.useFrameLayout(true)
+    }
+
+}
--- JSLayoutSizeFit.podspec
+++ JSLayoutSizeFit.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name         = "JSLayoutSizeFit"
-  s.version      = "0.2.3"
+  s.version      = "0.2.5"
   s.summary      = "JSLayoutSizeFit"
   s.homepage     = "https://github.com/jiasongs/JSLayoutSizeFit"
   s.author       = { "jiasong" => "593908937@qq.com" }
